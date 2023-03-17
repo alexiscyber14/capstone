761ff39e2f9d81bb02ad665d558469614ebf52f1
alexiscyber14 <142sledgehammer14@gmail.com>
2023-03-17 20:56:20 +0100

    add likes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ const navigation = require('./modules/navigation.js');
 
 const list = require('./modules/list.js');
 
+const liking = require('./modules/like.js');
+
 navigation();
 
 list();
+
+const observer = new MutationObserver(() => {
+  if (document.querySelectorAll('.movie-card')) {
+    liking();
+
+    observer.disconnect();
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+});
